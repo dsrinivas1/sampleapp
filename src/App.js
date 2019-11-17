6ef446a0e5619ea75411597542c363d601191d6c
dsrinivas1 <srinivasduggapu@gmail.com>
2019-11-17 17:57:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style = {{color: 'red'}}>
+
+      {"this is my first app"}
+          </div>
+  );
+}
+
+export default App;
